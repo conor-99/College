252d68aa1a740a197549adf77748386c0f67f4e0
conormccauley1999 <conormccauley1999@gmail.com>
2019-02-15 14:37:56 +0000

    Added project files

---
+++ CS2010/Assignment 1/SortComparison.java
@@ -0,0 +1,98 @@
+// -------------------------------------------------------------------------
+
+/**
+ *  This class contains static methods that implementing sorting of an array of numbers
+ *  using different sort algorithms.
+ *
+ *  @author
+ *  @version HT 2019
+ */
+
+ class SortComparison {
+
+    /**
+     * Sorts an array of doubles using InsertionSort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     * @param a: An unsorted array of doubles.
+     * @return array sorted in ascending order.
+     *
+     */
+    static double [] insertionSort (double a[]){
+
+        //todo: implement the sort
+    }//end insertionsort
+
+    /**
+     * Sorts an array of doubles using Quick Sort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     * @param a: An unsorted array of doubles.
+     * @return array sorted in ascending order
+     *
+     */
+    static double [] quickSort (double a[]){
+	
+		 //todo: implement the sort
+
+    }//end quicksort
+
+    /**
+     * Sorts an array of doubles using Merge Sort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     * @param a: An unsorted array of doubles.
+     * @return array sorted in ascending order
+     *
+     */
+    /**
+     * Sorts an array of doubles using iterative implementation of Merge Sort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     *
+     * @param a: An unsorted array of doubles.
+     * @return after the method returns, the array must be in ascending sorted order.
+     */
+
+    static double[] mergeSortIterative (double a[]) {
+
+		 //todo: implement the sort
+	
+    }//end mergesortIterative
+    
+    
+    
+    /**
+     * Sorts an array of doubles using recursive implementation of Merge Sort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     *
+     * @param a: An unsorted array of doubles.
+     * @return after the method returns, the array must be in ascending sorted order.
+     */
+    static double[] mergeSortRecursive (double a[]) {
+    	
+
+    	//todo: implement the sort
+	
+   }//end mergeSortRecursive
+    	
+    
+    /**
+     * Sorts an array of doubles using Selection Sort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     * @param a: An unsorted array of doubles.
+     * @return array sorted in ascending order
+     *
+     */
+    static double [] selectionSort (double a[]){
+
+         //todo: implement the sort
+
+    }//end selectionsort
+
+   
+
+
+    public static void main(String[] args) {
+
+        //todo: do experiments as per assignment instructions
+    }
+
+ }//end class
+
---
+++ CS2010/Assignment 1/SortComparisonTest.java
@@ -0,0 +1,51 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+//-------------------------------------------------------------------------
+/**
+ *  Test class for SortComparison.java
+ *
+ *  @author
+ *  @version HT 2019
+ */
+@RunWith(JUnit4.class)
+public class SortComparisonTest
+{
+    //~ Constructor ........................................................
+    @Test
+    public void testConstructor()
+    {
+        new SortComparison();
+    }
+
+    //~ Public Methods ........................................................
+
+    // ----------------------------------------------------------
+    /**
+     * Check that the methods work for empty arrays
+     */
+    @Test
+    public void testEmpty()
+    {
+    }
+
+
+    // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
+    // be executed at least once from at least one test.
+
+    // ----------------------------------------------------------
+    /**
+     *  Main Method.
+     *  Use this main method to create the experiments needed to answer the experimental performance questions of this assignment.
+     *
+     */
+    public static void main(String[] args)
+    {
+        //TODO: implement this method
+    }
+
+}
+
