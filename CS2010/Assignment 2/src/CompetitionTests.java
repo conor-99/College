729e099e1f839682a6b94a0b31e19e6e84466b67
conormccauley1999 <conormccauley1999@gmail.com>
2019-03-27 22:01:21 +0000

    Added files for second assignment

---
+++ CS2010/Assignment 2/src/CompetitionTests.java
@@ -0,0 +1,16 @@
+public class CompetitionTests {
+
+    @Test
+    public void testDijkstraConstructor() {
+
+        //TODO
+    }
+
+    @Test
+    public void testFWConstructor() {
+        //TODO
+    }
+
+    //TODO - more tests
+    
+}
