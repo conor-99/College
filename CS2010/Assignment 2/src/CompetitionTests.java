937ba84e7f86e52d189ef36684f81138f025fbe1
conormccauley1999 <conormccauley1999@gmail.com>
2019-04-09 21:10:06 +0100

    Finished Dijkstra stuff

--- CS2010/Assignment 2/src/CompetitionTests.java
+++ CS2010/Assignment 2/src/CompetitionTests.java
@@ -1,14 +1,53 @@
 import org.junit.Test;
+import org.junit.Assert;
 
 public class CompetitionTests {
 
     @Test
-    public void testDijkstraConstructor() {
+    public void testDijkstraTiny() {
+
+        CompetitionDijkstra dijkstra = new CompetitionDijkstra("inputs/tiny.txt", 50, 70, 80);
+
+        int expected = 38;
+        int actual = dijkstra.timeRequiredForCompetition();
+
+        Assert.assertEquals(expected, actual);
 
     }
 
     @Test
-    public void testFWConstructor() {
+    public void testDijkstraBig() {
+
+        CompetitionDijkstra dijkstra = new CompetitionDijkstra("inputs/big.txt", 60, 70, 80);
+
+        int expected = 24;
+        int actual = dijkstra.timeRequiredForCompetition();
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testFWTiny() {
+
+        CompetitionFloydWarshall floydWarshall = new CompetitionFloydWarshall("inputs/tiny.txt", 60, 70, 80);
+
+        int expected = -1;
+        int actual = floydWarshall.timeRequiredForCompetition();
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testFWBig() {
+
+        CompetitionFloydWarshall floydWarshall = new CompetitionFloydWarshall("inputs/big.txt", 60, 70, 80);
+
+        int expected = -1;
+        int actual = floydWarshall.timeRequiredForCompetition();
+
+        Assert.assertEquals(expected, actual);
 
     }
     
