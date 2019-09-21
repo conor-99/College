ddae4f7ccc8b6e72af3e11a5d3ca5eddf7a6a748
unknown <conormccauley1999@gmail.com>
2019-09-21 22:51:55 +0100

    Week 2 assignments

--- CS2022/Project 2/components_1b/datapath.vhd
+++ CS2022/Project 2/components_1b/datapath.vhd
@@ -6,10 +6,10 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity datapath is
 	port(
-		data_I, const_I: in std_logic_vector (15 downto 0);
+		data_I, const_I, PC_I: in std_logic_vector (15 downto 0);
 		F: in std_logic_vector (4 downto 0);
 		dst_A, src_A, src_B: in std_logic_vector (3 downto 0);
-		RW, MB, MD, Clk: in std_logic;
+		RW, MB, MD, MM, Clk: in std_logic;
 		data_O, addr_O: out std_logic_vector (15 downto 0);
 		V, C, N, Z: out std_logic
 	);
@@ -31,7 +31,7 @@ architecture behave of datapath is
 			src_A0, src_A1, src_A2, src_A3: in std_logic;
 			src_B0, src_B1, src_B2, src_B3: in std_logic;
 			des_A0, des_A1, des_A2, des_A3: in std_logic;
-			load, Clk: in std_logic;
+			Clk, RW: in std_logic;
 			data: in std_logic_vector (15 downto 0);
 			data_A, data_B: out std_logic_vector (15 downto 0)
 		);
@@ -50,6 +50,7 @@ architecture behave of datapath is
 	signal reg_A: std_logic_vector (15 downto 0);
 	signal muxd_O: std_logic_vector (15 downto 0);
 	signal func_O: std_logic_vector (15 downto 0);
+	signal muxm_O: std_logic_vector (15 downto 0);
 	
 begin
 	
@@ -59,13 +60,14 @@ begin
 		src_A(0), src_A(1), src_A(2), src_A(3),
 		src_B(0), src_B(1), src_B(2), src_B(3),
 		dst_A(0), dst_A(1), dst_A(2), dst_A(3),
-		RW, Clk, muxd_O, reg_A, reg_B
+		Clk, RW, muxd_O, reg_A, reg_B
 	);
 	
 	comp_muxb: mux2 port map (reg_B, const_I, MB, muxb_O);
 	comp_muxd: mux2 port map (func_O, data_I, MD, muxd_O);
+	comp_muxm: mux2 port map (reg_A, PC_I, MM, muxm_O);
 	
 	data_O <= muxb_O; 
-	addr_O <= reg_A;
+	addr_O <= muxm_O;
 	
 end behave;
---
+++ CSU34016/Exercise 0/Ex00.hs
@@ -0,0 +1,4 @@
+-- Name: Conor McCauley,  Username: mccaulco
+module Ex00 where
+
+hello =  "Hello World :-)"
