3073238e1dfc257f9676f05acdc2aaa000ecabe6
unknown <conormccauley1999@gmail.com>
2019-09-23 05:26:26 +0100

    Created project as well as very basic binary tree

---
+++ CSU33012/Task 1 - Lowest Common Ancestor/LowestCommonAncestor/BinaryTree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LowestCommonAncestor
+{
+
+    class BinaryTree<T>
+    {
+
+        BinaryTreeNode<T> root;
+
+        public BinaryTree(T value, BinaryTreeNode<T> left, BinaryTreeNode<T> right)
+        {
+            root = new BinaryTreeNode<T>(value, left, right);
+        }
+
+    }
+
+    class BinaryTreeNode<T>
+    {
+
+        public BinaryTreeNode<T> left, right;
+        public T value;
+
+        public BinaryTreeNode(T value, BinaryTreeNode<T> left, BinaryTreeNode<T> right)
+        {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+}
---
+++ CSU33012/Task 1 - Lowest Common Ancestor/LowestCommonAncestor/LowestCommonAncestor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LowestCommonAncestor
+{
+
+    class LowestCommonAncestor
+    {
+
+        
+
+    }
+
+}
