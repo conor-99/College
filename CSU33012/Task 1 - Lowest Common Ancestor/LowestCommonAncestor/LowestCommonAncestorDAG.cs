2ca2d36d0665e8bd0e7c45fa2b67bcdaf76cecb6
unknown <conormccauley1999@gmail.com>
2019-10-12 00:56:12 +0100

    Added branch for DAG and began work on it

---
+++ CSU33012/Task 1 - Lowest Common Ancestor/LowestCommonAncestor/LowestCommonAncestorDAG.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace LowestCommonAncestor
+{
+
+    public class DirectedAcyclicGraph
+    {
+
+        public const int NONE = int.MaxValue;
+
+        private List<DirectedAcyclicGraphVertex> vertices;
+
+        public void AddVertex(DirectedAcyclicGraphVertex vertex)
+        {
+            vertices.Add(vertex);
+        }
+
+        public int FindLCA(int a, int b)
+        {
+            return NONE;
+        }
+
+    }
+
+    public class DirectedAcyclicGraphVertex
+    {
+
+        public List<DirectedAcyclicGraphVertex> vertices;
+        public int value;
+
+        public DirectedAcyclicGraphVertex(int value)
+        {
+            this.value = value;
+        }
+
+        public void AddVertex(DirectedAcyclicGraphVertex vertex)
+        {
+            vertices.Add(vertex);
+        }
+
+    }
+
+}
