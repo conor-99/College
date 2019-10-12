9f0c76ddf3a875e6461d2e7eb039b18159196a6c
unknown <conormccauley1999@gmail.com>
2019-10-12 01:14:06 +0100

    Added stub tests

---
+++ CSU33012/Task 1 - Lowest Common Ancestor/LowestCommonAncestor/LowestCommonAncestorDAGTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace LowestCommonAncestor
+{
+
+    public class TestsDAG
+    {
+
+        [SetUp]
+        public void Setup() {}
+
+        [Test]
+        public void TestStandardDAG()
+        {
+
+            int result = 1;
+
+            Assert.AreEqual(1, result, $"{result} != 1");
+
+        }
+
+    }
+
+}
