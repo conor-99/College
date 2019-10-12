e89ea6d5fedc39a8d97d8440fe7a7e0c70567fa5
unknown <conormccauley1999@gmail.com>
2019-10-12 01:37:27 +0100

    Turns out I hadn't improved anything...

--- CSU33012/Task 1 - Lowest Common Ancestor/LowestCommonAncestor/LowestCommonAncestorDAG.cs
+++ CSU33012/Task 1 - Lowest Common Ancestor/LowestCommonAncestor/LowestCommonAncestorDAG.cs
@@ -8,24 +8,26 @@ namespace LowestCommonAncestor
 
         public const int NONE = int.MaxValue;
 
-        private Dictionary<int, HashSet<int>> vertices = new Dictionary<int, HashSet<int>>();
-
-        public void AddVertex(int value, HashSet<int> successors)
-        {
-
-            if (!vertices.ContainsKey(value))
-                vertices.Add(value, new HashSet<int>());
-
-            if (successors != null)
-                vertices[value].UnionWith(successors);
-            
-        }
+        public DirectedAcyclicGraphVertex root;
 
         public int FindLCA(int a, int b)
         {
             return NONE;
         }
         
+    }
+
+    public class DirectedAcyclicGraphVertex
+    {
+
+        public List<DirectedAcyclicGraphVertex> predecessors, successors;
+        public int value;
+
+        public DirectedAcyclicGraphVertex(int value)
+        {
+            this.value = value;
+        }
+
     }
 
 }
--- CSU33012/Task 1 - Lowest Common Ancestor/LowestCommonAncestor/LowestCommonAncestorDAGTest.cs
+++ CSU33012/Task 1 - Lowest Common Ancestor/LowestCommonAncestor/LowestCommonAncestorDAGTest.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
+using Vertex = LowestCommonAncestor.DirectedAcyclicGraphVertex; // makes the code a lot easier to read
 
 namespace LowestCommonAncestor
 {
@@ -16,16 +17,32 @@ namespace LowestCommonAncestor
 
             DirectedAcyclicGraph graph = new DirectedAcyclicGraph();
 
-            graph.AddVertex(1, new HashSet<int>() { 2, 4, 6 });
-            graph.AddVertex(2, new HashSet<int>() { 3, 5 });
-            graph.AddVertex(3, new HashSet<int>() { 4 });
-            graph.AddVertex(4, null);
-            graph.AddVertex(5, new HashSet<int>() { 3 });
-            graph.AddVertex(6, new HashSet<int>() { 2 });
+            Vertex v1, v2, v3, v4, v5, v6;
+            v1 = new Vertex(1);
+            v2 = new Vertex(2);
+            v3 = new Vertex(3);
+            v4 = new Vertex(4);
+            v5 = new Vertex(5);
+            v6 = new Vertex(6);
+
+            v1.predecessors = null;
+            v1.successors   = new List<Vertex>() { v2, v4, v6 };
+            v2.predecessors = new List<Vertex>() { v1, v6 };
+            v2.successors   = new List<Vertex>() { v3, v5 };
+            v3.predecessors = new List<Vertex>() { v2, v5 };
+            v3.successors   = new List<Vertex>() { v4 };
+            v4.predecessors = new List<Vertex>() { v1, v3 };
+            v4.successors   = null;
+            v5.predecessors = new List<Vertex>() { v2 };
+            v5.successors   = new List<Vertex>() { v3 };
+            v6.predecessors = new List<Vertex>() { v1 };
+            v6.successors   = new List<Vertex>() { v2 };
+
+            graph.root = v1;
 
             int result = graph.FindLCA(4, 5);
 
-            Assert.AreEqual(2, result, $"{result} != 2");
+            Assert.AreEqual(5, result, $"{result} != 5");
 
         }
 
