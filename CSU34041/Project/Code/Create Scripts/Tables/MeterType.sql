66dc8b0fa8a627ee8233851e5f6abfd3dcb35a1e
unknown <conormccauley1999@gmail.com>
2019-11-24 19:37:46 +0000

    Fix MeterType and add trigger for date columns

--- CSU34041/Project/Code/Create Scripts/Tables/MeterType.sql
+++ CSU34041/Project/Code/Create Scripts/Tables/MeterType.sql
@@ -5,5 +5,7 @@ create table if not exists MeterType (
 	Id int auto_increment primary key,
 	UnitId int,
 	`Name` varchar(255) not null unique,
+    DateCreated timestamp not null,
+	DateModified timestamp null,
     constraint FK_MeterType_Unit foreign key (UnitId) references Unit(Id)
 );
