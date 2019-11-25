46747d1e9cd53a9579582bb146cb8f7df4ad9316
unknown <conormccauley1999@gmail.com>
2019-11-25 20:59:03 +0000

    Email stuff done

--- CSU34041/Project/Code/Create Scripts/StoredProcedures/SendInvoiceEmail.sql
+++ CSU34041/Project/Code/Create Scripts/StoredProcedures/SendInvoiceEmail.sql
@@ -4,7 +4,21 @@ drop procedure if exists SendInvoiceEmail;
 delimiter //
 create procedure SendInvoiceEmail(in InvoiceId int)
 begin
-
+	
+    # Variables we'll fetch from the cursor
+    declare externalId varchar(20);
+    declare meterType, unitSymbol varchar(255);
+    declare originalValue, convertedValue, meterAmount, marketPrice real;
+    
+    # Meter cursor and flag
+    declare done bool default false;
+	declare meterCursor cursor for (
+		select ic.ExternalIdentifier, ic.MeterType, ic.OriginalValue, ic.UnitSymbol, ic.ConvertedValue, ic.MeterAmount, ic.MarketPrice
+        from InvoiceEmailContent ic
+        where ic.InvoiceId = InvoiceId
+	);
+    declare continue handler for not found set done = true;
+    
 	# Get some key information:
 	set @customerName = (select Customer from InvoiceEmailContent ic where ic.InvoiceId = InvoiceId limit 1);
 	set @invoiceAmount = (select InvoiceAmount from InvoiceEmailContent ic where ic.InvoiceId = InvoiceId limit 1);
@@ -23,14 +37,27 @@ begin
     # Generate start of email body
     set @emailBody = concat(
 		'Dear ', @customerName, ',\n',
-        'You are being charged €', @invoiceAmount, ' for energy used from ', @formatStartDate, ' to ', @formatEndDate, '.\n'
+        'You are being charged €', @invoiceAmount, ' for energy used from ', @formatStartDate, ' to ', @formatEndDate, '.\n\n'
 	);
     
     # Generate invoice breakdown for email body
-    # | @externalId (@meterType): @originalValue @unitSymbol => @convertedValue kWh => €@meterAmount (at @marketPrice € per kWh)
+    open meterCursor;
+    meterLoop: loop
+		fetch meterCursor into externalId, meterType, originalValue, unitSymbol, convertedValue, meterAmount, marketPrice;
+        if done then leave meterLoop; end if;
+        set @emailBody = concat(
+			@emailBody,
+            externalId, ' (', meterType, '): ', originalValue, ' (', unitSymbol, ') => ', convertedValue, ' (kWh) => €', meterAmount, ' (at ', marketPrice, ' € per kWh)\n'
+		);
+    end loop;
+    close meterCursor;
     
+    # Insert the email into the queue
     insert into EmailQueue (IsSent, Recipient, `Subject`, Body)
     select 0, @emailRecipient, @emailSubject, @emailBody;
+    
+    # Set the invoice to sent
+    update Invoice set InvoiceStatusId = 1 where Id = InvoiceId;
     
 end //
 delimiter ;
