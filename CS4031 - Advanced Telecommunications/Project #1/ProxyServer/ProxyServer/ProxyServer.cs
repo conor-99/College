ceddcc5999beeea68e1f90db813069697e2b8709
unknown <conormccauley1999@gmail.com>
2020-02-10 00:10:11 +0000

    Created project for telecoms project

---
+++ CS4031 - Advanced Telecommunications/Project #1/ProxyServer/ProxyServer/ProxyServer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProxyServer
+{
+    public partial class ProxyServer : Form
+    {
+        public ProxyServer()
+        {
+            InitializeComponent();
+        }
+    }
+}
