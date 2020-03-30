585b001955aa9f278580f9ba2815c9ee5ae08568
conormccauley1999 <conormccauley1999@gmail.com>
2020-03-31 00:22:26 +0100

    Rename to save column space on report

--- ST3009 - Statistical Methods for Computer Science/Mid-Term Assignment/code.py
+++ ST3009 - Statistical Methods for Computer Science/Mid-Term Assignment/code.py
@@ -2,4 +2,4 @@ def parse_timings():
 	return tuple(zip(*[list(map(int, line.strip().split())) for line in open("dataset_timings.txt").readlines()]))
 
 def parse_probabilities():
-	return tuple(map(float, open("dataset_probabilities.txt").readline().split()))
+	return tuple(map(float, open("dataset_probs.txt").readline().split()))
