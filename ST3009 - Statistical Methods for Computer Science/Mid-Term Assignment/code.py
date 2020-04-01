2a5fad1254330d9b62a5d39e322bde3a4cf51582
conormccauley1999 <conormccauley1999@gmail.com>
2020-04-01 12:03:20 +0100

    Most of Q1 and all of Q2 done

--- ST3009 - Statistical Methods for Computer Science/Mid-Term Assignment/code.py
+++ ST3009 - Statistical Methods for Computer Science/Mid-Term Assignment/code.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 def parse_timings():
 	return tuple(zip(*[list(map(int, line.strip().split())) for line in open("dataset_timings.txt").readlines()]))
 
@@ -5,10 +7,31 @@ def parse_probabilities():
 	return tuple(map(float, open("dataset_probabilities.txt").readline().split()))
 
 def pmf_hist(timings):
+	plt.hist(timings, 50, density=True)
+	plt.show()
+
+def prob(timings):
+	rv = lambda x: 1 if x > 10 else 0
+	count = sum(1 if rv(timing) == 1 else 0 for timing in timings)
+	return count / len(timings)
+
+def conf_intervals(timings):
 	return
 
-def prob(timings, value):
-	return
 
-def conf_intervals(timings, value):
-	return
+# Data
+all_timings = parse_timings()
+probs = parse_probabilities()
+
+# Q1(a)
+pmf_hist(all_timings[0])
+
+# Q1(b)
+print(prob(all_timings[0]))
+
+# Q1(c)
+conf_intervals(all_timings[0])
+
+# Q2
+for timings in all_timings[1:]:
+	print(prob(timings))
